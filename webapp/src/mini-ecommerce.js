84b445f9f75befad8affa4d3158275f52b2f304c
Victor-Matheus <victormatheusct@gmail.com>
2020-07-20 13:28:42 -0300

    added a initial menu component

--- webapp/src/mini-ecommerce.js
+++ webapp/src/mini-ecommerce.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import './mini-ecommerce.css';
+import Menu from './components/menu/Menu';
 
 function MiniEcommerce() {
+
+  const [carriho, setCarrinho] = React.useState({products : []});
+  const [exibirProdutos, setExibirProdutos] = React.useState(true);
+  const [exibirCheckOut, setExibirCheckOut] = React.useState(true);
+  const [valorTotal, setValorTotal] = React.useState('0,00');
+
   return (
-    <h1>Mini-ecommerce</h1>
+    <div>
+      <Menu />
+    </div>
   );
 }
 
